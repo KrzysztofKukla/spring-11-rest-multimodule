d3f33cc4cde5010e66819c55b3ab6e73df51c3ec
Krzysztof Kukla <krzys.kukla@gmail.com>
2020-04-27 00:31:25 +0200

    432 testing with using .classed generated from xsd schema file

--- rest-app/src/test/java/pl/kukla/krzys/spring09rest/service/CustomerServiceImplIT.java
+++ rest-app/src/test/java/pl/kukla/krzys/spring09rest/service/CustomerServiceImplIT.java
@@ -1,10 +1,20 @@
 package pl.kukla.krzys.spring09rest.service;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import pl.kukla.krzys.spring09rest.domain.CustomerDto;
+import pl.kukla.krzys.spring09rest.exception.CustomerNotFoundException;
 import pl.kukla.krzys.spring09rest.repository.CustomerRepository;
 import pl.kukla.krzys.spring09rest.web.mapper.CustomerMapperImpl;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author Krzysztof Kukla
@@ -26,56 +36,67 @@ class CustomerServiceImplIT {
         customerRepository.deleteAll();
     }
 
-//    @Test
-//    void findAllCustomers() throws Exception {
-//        List<CustomerDto> customers = customerService.findAll();
-//
-//        Assertions.assertThat(customers).isEmpty();
-//    }
-//
-//    @Test
-//    void createCustomer() throws Exception {
-//        Assumptions.assumeTrue(customerRepository.count() == 0);
-//        CustomerDto customerDto = CustomerDto.builder().firstName("first created").lastName("last created").build();
-//
-//        CustomerDto savedCustomer = customerService.createCustomer(customerDto);
-//
-//        assertEquals(1, customerRepository.count());
-//        assertAll(
-//            () -> assertEquals(customerDto.getFirstName(), savedCustomer.getFirstName()),
-//            () -> assertEquals(customerDto.getLastName(), savedCustomer.getLastName())
-//        );
-//    }
-//
-//    @Test
-//    void updateCustomer() throws Exception {
-//        Assumptions.assumeTrue(customerRepository.count() == 0);
-//        Customer customerToSave = Customer.builder().firstName("Chris").lastName("kukla").build();
-//        CustomerDto customerPreparedToUpdate = CustomerDto.builder().firstName("first created").lastName("last created").build();
-//
-//        Customer savedCustomer = customerRepository.save(customerToSave);
-//        Assumptions.assumeTrue(customerRepository.count() == 1);
-//
-//        CustomerDto updatedCustomer = customerService.updateCustomer(savedCustomer.getId(), customerPreparedToUpdate);
-//
-//        assertEquals(1, customerRepository.count());
-//        assertAll(
-//            () -> assertEquals(customerPreparedToUpdate.getFirstName(), updatedCustomer.getFirstName()),
-//            () -> assertEquals(customerPreparedToUpdate.getLastName(), updatedCustomer.getLastName())
-//        );
-//    }
-//
-//    @Test
-//    void deleteCustomer() throws Exception {
-//        Assumptions.assumeTrue(customerRepository.count() == 0);
-//        Customer customerToSave = Customer.builder().firstName("Chris").lastName("kukla").build();
-//        CustomerDto customerPreparedToUpdate = CustomerDto.builder().firstName("first created").lastName("last created").build();
-//
-//        Customer savedCustomer = customerRepository.save(customerToSave);
-//        Assumptions.assumeTrue(customerRepository.count() == 1);
-//
-//        customerService.deleteCustomer(savedCustomer.getId());
-//        assertEquals(0, customerRepository.count());
-//    }
+    @Test
+    void findAllCustomers() throws Exception {
+        List<CustomerDto> customers = customerService.findAll();
+
+        Assertions.assertThat(customers).isEmpty();
+    }
+
+    @Test
+    void createCustomer() throws Exception {
+        Assumptions.assumeTrue(customerRepository.count() == 0);
+
+        CustomerDto customerDto = createCustomerDto("first created","last created");
+        CustomerDto savedCustomer = customerService.createCustomer(customerDto);
+
+        assertEquals(1, customerRepository.count());
+        assertAll(
+            () -> assertEquals(customerDto.getFirstName(), savedCustomer.getFirstName()),
+            () -> assertEquals(customerDto.getLastName(), savedCustomer.getLastName())
+        );
+    }
+
+    @Test
+    void updateCustomer() throws Exception {
+        Assumptions.assumeTrue(customerRepository.count() == 0);
+        CustomerDto customerToSave = createCustomerDto("Chris","kukla");
+        CustomerDto customerPreparedToUpdate = createCustomerDto("first created","last created");
+
+        CustomerDto savedCustomer = customerService.createCustomer(customerToSave);
+        Assumptions.assumeTrue(customerRepository.count() == 1);
+        Long id = customerRepository.findAll().stream().findFirst()
+            .orElseThrow(() -> new CustomerNotFoundException("Cannot find any Customer")).getId();
+
+        CustomerDto updatedCustomer = customerService.updateCustomer(id, customerPreparedToUpdate);
+
+        assertEquals(1, customerRepository.count());
+        assertAll(
+            () -> assertEquals(customerPreparedToUpdate.getFirstName(), updatedCustomer.getFirstName()),
+            () -> assertEquals(customerPreparedToUpdate.getLastName(), updatedCustomer.getLastName())
+        );
+    }
+
+    @Test
+    void deleteCustomer() throws Exception {
+        Assumptions.assumeTrue(customerRepository.count() == 0);
+        CustomerDto customerToSave = createCustomerDto("Chris", "kukla");
+        CustomerDto customerPreparedToUpdate = createCustomerDto("first created","last created");
+
+        CustomerDto savedCustomer = customerService.createCustomer(customerToSave);
+        Assumptions.assumeTrue(customerRepository.count() == 1);
+
+        Long id = customerRepository.findAll().stream().findFirst()
+            .orElseThrow(() -> new CustomerNotFoundException("Cannot find any Customer")).getId();
+        customerService.deleteCustomer(id);
+        assertEquals(0, customerRepository.count());
+    }
+
+    private CustomerDto createCustomerDto(String firstName, String lastName) {
+        CustomerDto customerDto = new CustomerDto();
+        customerDto.setFirstName(firstName);
+        customerDto.setLastName(lastName);
+        return customerDto;
+    }
 
 }
--- rest-app/src/test/java/pl/kukla/krzys/spring09rest/service/CustomerServiceImplTest.java
+++ rest-app/src/test/java/pl/kukla/krzys/spring09rest/service/CustomerServiceImplTest.java
@@ -1,11 +1,21 @@
 package pl.kukla.krzys.spring09rest.service;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import pl.kukla.krzys.spring09rest.domain.Customer;
+import pl.kukla.krzys.spring09rest.domain.CustomerDto;
 import pl.kukla.krzys.spring09rest.repository.CustomerRepository;
 import pl.kukla.krzys.spring09rest.web.mapper.CustomerMapper;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 
 /**
  * @author Krzysztof Kukla
@@ -22,42 +32,49 @@ class CustomerServiceImplTest {
     @InjectMocks
     private CustomerServiceImpl customerService;
 
-//    @Test
-//    void createCustomer() {
-//        String firstName = "first Name";
-//        String lastName = "first lastName";
-//        CustomerDto customerDto = CustomerDto.builder().firstName(firstName).lastName(lastName).build();
-//        Customer customer = Customer.builder().firstName(firstName).lastName(lastName).build();
-//
-//        BDDMockito.when(customerMapper.customerDtoToCustomer(any(CustomerDto.class))).thenReturn(customer);
-//        BDDMockito.when(customerRepository.save(any(Customer.class))).thenReturn(customer);
-//
-//        CustomerDto savedCustomer = customerService.createCustomer(customerDto);
-//
-//        Assertions.assertEquals(customer.getLastName(), savedCustomer.getLastName());
-//        BDDMockito.then(customerMapper).should().customerDtoToCustomer(any(CustomerDto.class));
-//        BDDMockito.then(customerRepository).should().save(any(Customer.class));
-//    }
-//
-//    @Test
-//    void updateCustomer() throws Exception {
-//        Long id = 1L;
-//        String firstName = "first Name";
-//        String lastName = "first lastName";
-//        Customer customerToUpdate = Customer.builder().id(1L).firstName("f").lastName("l").build();
-//        CustomerDto customerDto = CustomerDto.builder().firstName(firstName).lastName(lastName).build();
-//        Customer updatedCustomer = Customer.builder().id(id).firstName(firstName).lastName(lastName).build();
-//
-//        BDDMockito.when(customerRepository.findById(anyLong())).thenReturn(Optional.of(customerToUpdate));
-//        BDDMockito.when(customerMapper.customerDtoToCustomer(any(CustomerDto.class))).thenReturn(updatedCustomer);
-//        BDDMockito.when(customerRepository.save(any(Customer.class))).thenReturn(updatedCustomer);
-//
-//        CustomerDto updatedCustomerResponse = customerService.updateCustomer(id, customerDto);
-//
-//        Assertions.assertEquals(updatedCustomer.getLastName(), updatedCustomerResponse.getLastName());
-//        BDDMockito.then(customerRepository).should().findById(anyLong());
-//        BDDMockito.then(customerMapper).should().customerDtoToCustomer(any(CustomerDto.class));
-//        BDDMockito.then(customerRepository).should().save(any(Customer.class));
-//    }
+    @Test
+    void createCustomer() {
+        String firstName = "first Name";
+        String lastName = "first lastName";
+        CustomerDto customerDto = createCustomerDto(firstName,lastName);
+        Customer customer = Customer.builder().firstName(firstName).lastName(lastName).build();
+
+        BDDMockito.when(customerMapper.customerDtoToCustomer(any(CustomerDto.class))).thenReturn(customer);
+        BDDMockito.when(customerRepository.save(any(Customer.class))).thenReturn(customer);
+
+        pl.kukla.krzys.spring09rest.domain.CustomerDto savedCustomer = customerService.createCustomer(customerDto);
+
+        Assertions.assertEquals(customer.getLastName(), savedCustomer.getLastName());
+        BDDMockito.then(customerMapper).should().customerDtoToCustomer(any(CustomerDto.class));
+        BDDMockito.then(customerRepository).should().save(any(Customer.class));
+    }
+
+    @Test
+    void updateCustomer() throws Exception {
+        Long id = 1L;
+        String firstName = "first Name";
+        String lastName = "first lastName";
+        Customer customerToUpdate = Customer.builder().id(1L).firstName("f").lastName("l").build();
+        CustomerDto customerDto = createCustomerDto(firstName,lastName);
+        Customer updatedCustomer = Customer.builder().id(id).firstName(firstName).lastName(lastName).build();
+
+        BDDMockito.when(customerRepository.findById(anyLong())).thenReturn(Optional.of(customerToUpdate));
+        BDDMockito.when(customerMapper.customerDtoToCustomer(any(CustomerDto.class))).thenReturn(updatedCustomer);
+        BDDMockito.when(customerRepository.save(any(Customer.class))).thenReturn(updatedCustomer);
+
+        CustomerDto updatedCustomerResponse = customerService.updateCustomer(id, customerDto);
+
+        Assertions.assertEquals(updatedCustomer.getLastName(), updatedCustomerResponse.getLastName());
+        BDDMockito.then(customerRepository).should().findById(anyLong());
+        BDDMockito.then(customerMapper).should().customerDtoToCustomer(any(CustomerDto.class));
+        BDDMockito.then(customerRepository).should().save(any(Customer.class));
+    }
+
+    private CustomerDto createCustomerDto(String firstName, String lastName) {
+        pl.kukla.krzys.spring09rest.domain.CustomerDto customerDto = new pl.kukla.krzys.spring09rest.domain.CustomerDto();
+        customerDto.setFirstName(firstName);
+        customerDto.setLastName(lastName);
+        return customerDto;
+    }
 
 }
